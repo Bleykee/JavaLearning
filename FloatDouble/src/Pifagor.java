076718437745138361a96be898e31695ad8a1d21
Eugene Ulnyrov <89982601+Bleykee@users.noreply.github.com>
2021-09-05 01:08:17 +0300

    Add files via upload

---
+++ FloatDouble/src/Pifagor.java
@@ -0,0 +1,10 @@
+public class Pifagor {
+    public static double pow(double a){
+     return a*a;
+    }
+    public static void main(String[] args) {
+    float a = 3f;
+    float b = 3f;
+        System.out.println(pow(a*b));
+    }
+}
---
+++ FloatDouble/src/Square.java
@@ -0,0 +1,8 @@
+public class Square {
+    public static void main(String[] args) {
+        float r = 10.8f;
+        float pi = 3.14f;
+        float s = pi * r * r;
+        System.out.println("Площадь круга равна: "+ s);
+    }
+}
