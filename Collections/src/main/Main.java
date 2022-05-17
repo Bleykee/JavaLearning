ebb6625324157c8e8106c8d380ae42ea15eda3ba
Eugene Ulnyrov <89982601+Bleykee@users.noreply.github.com>
2022-05-17 18:27:29 +0300

    Add files via upload
    Collections(new)

--- Collections/src/main/Main.java
+++ Collections/src/main/Main.java
@@ -2,18 +2,16 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
-       MyArrayList employees = new MyArrayList();
-       employees.add("Ваня");
-       employees.add("Игорь");
-       employees.add("Петя");
-       employees.add("Ирина");
-       employees.add("Женя");
-       employees.add("Антон");
-       employees.add("Кристина");
-       employees.remove("Петя");
-       for (int i = 0; i < employees.getSize(); i++){
-           System.out.println(employees.get(i));
-       }
-
+        MyArrayList employees = new MyArrayList();
+        employees.add("Никита");
+        employees.add("Женя");
+        employees.add("Артём");
+        employees.add("Вика");
+        employees.add("Оля");
+        employees.add("Влад");
+        employees.remove("Влад");
+        for (int i = 0; i < employees.getSize(); i++){
+            System.out.println(employees.get(i));
+        }
     }
 }
--- Collections/src/main/MyArrayList.java
+++ Collections/src/main/MyArrayList.java
@@ -1,44 +1,48 @@
 package main;
 
 public class MyArrayList {
-    private String[] array = new String [10];
-    private int size = 0;
-    public void add (String s){
-        array[size] = s;
-        size++;
-        if (size == array.length){
-            String[] newArray = new String [array.length * 2];
-            for (int i = 0; i < array.length; i++){
-                newArray[i] = array [i];
+    private String[] array = new String[10]; //изначальный размер массива
+    private int size = 0; //переменная, которая хранит в себе кол-во элементов
+
+    public void add(String s) { //метод, принимающий параметр типа String
+        array[size] = s; //берем строку и добавляем в нее массив с индексом size
+        size++; //в массиве появляется новый элемент, поэтому увеличиваем size на 1
+        if (size == array.length) { //проверяем, не стала ли size равна длине массива. Если стала, увеличиваем длину массива вдвое
+            String[] newArray = new String[array.length * 2];
+            for (int i = 0; i < array.length; i++) { // копируем все элементы из первого массива во второй
+                newArray[i] = array[i];
             }
-            array = newArray;
+            array = newArray; //переменной array присваеваем значения нового массива
         }
     }
-    public void remove (int index){
-        if (index >= 0 && index < size) {
+
+    public void remove(int index) {
+        if (index >= 0 && index < size) { //Проверяем, чтобы введенное число было правильным
             for (int i = index; i < size - 1; i++) {
                 array[i] = array[i + 1];
             }
             size--;
         }
     }
-    public void remove (String s){
+
+    public void remove(String s){
         int index = -1;
         for (int i = 0; i < size; i++){
-            if (s.equals(array[i])) {
+            if (s.equals(array[i])){
                 index = i;
                 break;
             }
         }
-      if (index != -1){
-          remove(index);
-      }
+        if (index != -1){
+            remove(index);
+        }
     }
 
-    public int getSize() {
+    public int getSize() { //геттер, чтобы знать кол-во элементов в массиве
         return size;
     }
-    public String get (int index){
+
+    public String get(int index) {
         if (index >= 0 && index < size) {
             return array[index];
         } else {
