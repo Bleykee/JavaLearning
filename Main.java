ae847ed2f5a2e0b6f9f7fc808c286d9b99d5ae15
Eugene Ulnyrov <89982601+Bleykee@users.noreply.github.com>
2022-05-17 17:45:51 +0300

    Add files via upload
    Static, final

---
+++ Main.java
@@ -0,0 +1,12 @@
+package main;
+
+
+import test.MyMath;
+
+public class Main {
+    public static void main(String[] args) {
+        int sum = MyMath.sum(1,4,5,6,67,3,2,2,426,6);
+        System.out.println(sum);
+    }
+
+    }
